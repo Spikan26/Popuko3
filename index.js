cad7cb11892d6fda9ce8975bcac3bb329e0cc6e0
alexandre.durieux <durieuxalexandre26@gmail.com>
2018-09-27 18:48:07 +0200

    Command //Gitout ready !

--- index.js
+++ index.js
@@ -29,6 +29,42 @@ bot.on("ready", function () {
 })
 
 
+bot.on("message", async message => {
+	
+	if (message.author.equals(bot.user)) return;
+	
+	if (message.content.startsWith("//gitout")){
+		if(message.author.id != 178483636671086592){
+					message.channel.send("You can't do that !");
+					return
+				} else {
+					// Make sure the bot user has permissions to make channels and move members in the guild:
+					if (!message.guild.me.hasPermission(['MANAGE_CHANNELS', 'MOVE_MEMBERS'])) return message.reply('Missing the required `Manage Channels` and `Move Members` permissions.');
+					 
+					// Get the mentioned user/bot and check if they're in a voice channel:
+					const member = message.mentions.members.first();
+					if (!member) return message.reply('You need to @mention a user/bot to kick from the voice channel.');
+					if (!member.voiceChannel) return message.reply('That user/bot isn\'t in a voice channel.');
+					 
+					// Now we make a temporary voice channel, move the user/bot into the channel, and delete it:
+					const guild = message.guild;
+					const temp_channel = await message.guild.createChannel(member.id, 'voice', [
+					  { id: guild.id,
+						deny: ['VIEW_CHANNEL', 'CONNECT', 'SPEAK'], },
+					  { id: member.id,
+						deny: ['VIEW_CHANNEL', 'CONNECT', 'SPEAK'] }
+					]);
+					await member.setVoiceChannel(temp_channel);
+					 
+					await temp_channel.delete();
+					message.react('🚪');
+				}
+	
+	
+	}
+});
+
+
 bot.on("message", function (message) {
     
     if (message.author.equals(bot.user)) return;
@@ -126,34 +162,6 @@ bot.on("message", function (message) {
 					}
 				}
 				break
-				
-				
-			/*case "gitout":
-				if(message.author.id != 178483636671086592){
-					message.channel.send("You can't do that !");
-					return
-				} else {
-					// Make sure the bot user has permissions to make channels and move members in the guild:
-					if (!message.guild.me.hasPermission(['MANAGE_CHANNELS', 'MOVE_MEMBERS'])) return message.reply('Missing the required `Manage Channels` and `Move Members` permissions.');
-					 
-					// Get the mentioned user/bot and check if they're in a voice channel:
-					const member = message.mentions.members.first();
-					if (!member) return message.reply('You need to @mention a user/bot to kick from the voice channel.');
-					if (!member.voiceChannel) return message.reply('That user/bot isn\'t in a voice channel.');
-					 
-					// Now we make a temporary voice channel, move the user/bot into the channel, and delete it:
-					const temp_channel = await message.guild.createChannel(user.id, 'voice', [
-					  { id: guild.id,
-						deny: ['VIEW_CHANNEL', 'CONNECT', 'SPEAK'], },
-					  { id: member.id,
-						deny: ['VIEW_CHANNEL', 'CONNECT', 'SPEAK'] }
-					]);
-					await member.setVoiceChannel(temp_channel);
-					 
-					await temp_channel.delete();
-					message.reply("Done");
-				}
-				break*/
 				
 				
 			/*case "up":
