005c75155139987010ca9c07f02027f6ce278e5e
alexandre.durieux <durieuxalexandre26@gmail.com>
2018-09-28 12:02:32 +0200

    .

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ bot.on("message", async message => {
 			
 			case "list":
 				msgembed = "";
-				for(var i=0;i<200; i++){
+				for(var i=0;i<60; i++){
 					msgembed += ListEmoji[i]
 					i++
 				}
